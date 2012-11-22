aaf7bc023fde8059beccf44d77703f53a7a25c7c
Roberto Esposito <boborbt@gmail.com>
2012-11-22 11:42:43 +0100

    Improved command line interface so to allow using command prefixes.

--- src/Programs/Minerule/minerule.cpp
+++ src/Programs/Minerule/minerule.cpp
@@ -11,10 +11,19 @@
 
 using namespace minerule;
 
-void execSubCommand( std::string cmd, int argc, char** argv ) {
+std::string buildPath(std::string cmdName) {
+	size_t last_slash_pos = cmdName.find_last_of("/");
+	if(last_slash_pos == std::string::npos)
+		return "";
+	
+	return cmdName.substr(0, last_slash_pos+1);
+}
+
+
+void execSubCommand( std::string cmd, int argc, char** argv, std::string cmdPath ) {
 	const char** cmdArgv = (const char**) malloc( sizeof(char*[argc+2]) );
 	assert(cmdArgv != NULL );
-	std::string executableName = std::string("mr-")+cmd;
+	std::string executableName = cmdPath + std::string("mr-")+cmd;
 	
 	cmdArgv[0] = executableName.c_str();
 	for(size_t i=1; i<argc+1; ++i) {
@@ -25,17 +34,32 @@ void execSubCommand( std::string cmd, int argc, char** argv ) {
 	errno = 0;
 	execvp(executableName.c_str(), (char* const*) cmdArgv);
 	if(errno!=0) {
-		MRLog() << strerror(errno) << std::endl;
-		exit(10);
+		MRErr() << "Could not launch command:" + executableName << std::endl
+			<< "       The system reported error: " << strerror(errno) << std::endl;
+		exit(1);
 	}
 }
 
 void printMRHelp() {
 	std::cout << StringUtils::to_bold("Usage:") << std::endl
 		<< StringUtils::to_bold("mr [-h] <sub-command> [-h] [options]") << std::endl
-		<< "   Available sub-commands are: query, print, catalogue, and defaults" << std::endl
+		<< "   Available sub-commands are: query, print, catalogue, and defaults." << std::endl
+		<< StringUtils::to_bold("   Note:") << "Sub-commands can be shortened at will" << std::endl
+		<<                      "        e.g.: 'mr pr' is equivalent to 'mr print'" << std::endl
 		<< StringUtils::to_bold("   -h ") << " -- shows help for the given command if given after a sub-command name." << std::endl
 		<< "          it shows this message if given right after the 'mr' command" << std::endl;
+}
+
+
+std::string expandSubCommand(std::string cmdPrefix) {
+	static std::string commands[] = { "query", "catalogue", "print", "defaults" };
+	
+	for(size_t i=0; i<4; ++i) {
+		if( commands[i].find(cmdPrefix) == 0 ) // cmdPrefix is a prefix of commands[i]
+			return commands[i];
+	}
+	
+	return cmdPrefix;
 }
 
 
@@ -48,21 +72,21 @@ void checkSubCommands(int argc, char** argv) {
 	if(argc < 2) 
 		throw MineruleException( MR_ERROR_OPTION_PARSING, "Command not recognized, see help for a list of available commands." );
 	
-	std::string subCmd( argv[1] );
+	std::string subCmd( expandSubCommand(argv[1]) );
 	if(subCmd == "print") {
-		execSubCommand(subCmd, argc-2, &argv[2]);
+		execSubCommand(subCmd, argc-2, &argv[2], buildPath(argv[0]));
 	}
 	
 	if(subCmd == "catalogue") {
-		execSubCommand(subCmd, argc-2, &argv[2]);
+		execSubCommand(subCmd, argc-2, &argv[2], buildPath(argv[0]));
 	}
 	
 	if(subCmd == "defaults") {
-		execSubCommand(subCmd, argc-2, &argv[2]);
+		execSubCommand(subCmd, argc-2, &argv[2], buildPath(argv[0]));
 	}
 	
 	if(subCmd == "query") {
-		execSubCommand(subCmd, argc-2, &argv[2]);
+		execSubCommand(subCmd, argc-2, &argv[2], buildPath(argv[0]));
 	}
 	
 	throw MineruleException( MR_ERROR_OPTION_PARSING, "Command not recognized, see help for a list of available commands." );

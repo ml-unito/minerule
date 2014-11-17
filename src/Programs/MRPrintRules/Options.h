3515a4017264256432f384168876c7c1f13354d8
Roberto Esposito <boborbt@gmail.com>
2014-11-17 16:29:20 +0100

    Code cleanup (indentation, comments, ...).

--- src/Programs/MRPrintRules/Options.h
+++ src/Programs/MRPrintRules/Options.h
@@ -21,17 +21,25 @@
 
 namespace mrprint {
 	enum MRPRINT_ERRORS { MRPRINT_OPTION_PARSING_ERROR=1 };
-	
+
 	void printError(const std::string& str);
-	
+
 	class Options	{
 	public:
-		Options(int argc, char** argv) : _argc(argc), _argv(argv), _mroptFileName(minerule::MineruleOptions::DEFAULT_FILE_NAME), _noLogArtifacts(false), _noLowConfidenceFilter(false), _sortOrder("no"), _queryNumber(0), _queryName("") {};
+		Options(int argc, char** argv) :
+				_argv(argv),
+				_argc(argc),
+				_mroptFileName(minerule::MineruleOptions::DEFAULT_FILE_NAME),
+				_noLogArtifacts(false),
+				_noLowConfidenceFilter(false),
+				_sortOrder("no"),
+				_queryNumber(0),
+				_queryName("") {};
 		virtual ~Options () {};
-		
+
 		void parse();
 		void printHelp() const;
-		
+
 		// accessors
 		std::string mroptFileName() const { return _mroptFileName; }
 		bool noLogArtifacts() const { return _noLogArtifacts; }
@@ -40,11 +48,11 @@ namespace mrprint {
 		size_t queryNumber() const { return _queryNumber; }
 		std::string queryName() const { return _queryName; }
 
-				
+
 	private:
 		char** _argv;
 		int _argc;
-		
+
 		std::string _mroptFileName;
 		bool _noLogArtifacts;
 		bool _noLowConfidenceFilter;
@@ -54,6 +62,6 @@ namespace mrprint {
 	};
 
 
-} 
+}
 
 #endif /* end of include guard: OPTIONS_H_IK79Q6DV */
--- src/Result/QueryResult.cpp
+++ src/Result/QueryResult.cpp
@@ -19,78 +19,82 @@
 
 namespace minerule {
 
-	void 
-		QueryResult::Iterator::readElems(int id, ItemSet& elems, odbc::PreparedStatement* ps_elems) 
-	throw (MineruleException, odbc::SQLException, std::exception) {
-		ps_elems->setInt(1,id);
-		odbc::ResultSet* rs = ps_elems->executeQuery();
+  void
+  QueryResult::Iterator::readElems(int id, ItemSet& elems, odbc::PreparedStatement* ps_elems)
+  throw (MineruleException, odbc::SQLException, std::exception) {
 
-		try {
-			while( rs->next() ) {
-				std::string str = rs->getString(1);
-				ItemType elem;
-				elem.setSourceRowElement(*SourceRowElement::deserializeElementFromResultSet(rs,2));
-				elems.push_back(elem);
-			}
-		} catch (...) {
-			delete rs;
-			throw;
-		}
-  
-		delete rs;
-	}
+    // FIXME: THE MEMORY ERROR SHOULD BE DUE TO ONE OF THESE TWO LINES (COMMENTING OUT
+    //    THEM FIXES THE PROBLEM, COMMENTING OUT ONLY THE REST OF THE FUNCTION DOES NOT).
+    //    --> BUG IN LIBODBC++?
+    ps_elems->setInt(1,id);
+    odbc::ResultSet* rs = ps_elems->executeQuery();
 
+    try {
+      while( rs->next() ) {
+        ItemType elem;
+        elem.setSourceRowElement(*SourceRowElement::deserializeElementFromResultSet(rs,2));
 
-	void
-		QueryResult::Iterator::init( const std::string& rulesTable,
-			double support,
-	double confidence ) throw( MineruleException, odbc::SQLException ) {
-		odbc::Connection* odbc_conn = MineruleOptions::getSharedOptions().getODBC().getODBCConnection();
-		Connection connection;
-		connection.setOutTableName(rulesTable);
-			
-		state=odbc_conn->createStatement();
-		std::string query = 
-			"SELECT bodyId, headId, supp, con "
-				"FROM " + connection.getTableName(Connection::RulesTable) + " "
-					"WHERE supp>=" + Converter(support).toString() + " AND "
-						"con>="+Converter(confidence).toString();
+        elems.push_back(elem);
+      }
+    } catch (...) {
+      delete rs;
+      throw;
+    }
 
-		body_elems = odbc_conn->prepareStatement(
-			"SELECT * FROM "+ connection.getTableName(Connection::BodiesTable) +
-				" WHERE id=?");
-
-		head_elems = odbc_conn->prepareStatement(
-			"SELECT * FROM "+ connection.getTableName(Connection::HeadsTable) +
-				" WHERE id=?");
+    delete rs;
+  }
 
 
-		MRDebug() << "QueryResult::Iterator, the filter query is:" << query << std::endl;
+  void
+  QueryResult::Iterator::init( const std::string& rulesTable,
+  double support,
+  double confidence ) throw( MineruleException, odbc::SQLException ) {
+    odbc::Connection* odbc_conn = MineruleOptions::getSharedOptions().getODBC().getODBCConnection();
+    Connection connection;
+    connection.setOutTableName(rulesTable);
 
-		rs_rules = state->executeQuery(query);
-	}
+    state=odbc_conn->createStatement();
+    std::string query =
+    "SELECT bodyId, headId, supp, con "
+    "FROM " + connection.getTableName(Connection::RulesTable) + " "
+    "WHERE supp>=" + Converter(support).toString() + " AND "
+    "con>="+Converter(confidence).toString();
 
-	bool QueryResult::Iterator::next() {
-		return rs_rules->next();
-	}
+    body_elems = odbc_conn->prepareStatement(
+    "SELECT * FROM "+ connection.getTableName(Connection::BodiesTable) +
+    " WHERE id=?");
 
-	void QueryResult::Iterator::getRule( Rule& r ) throw(MineruleException, odbc::SQLException, std::exception) {
-		ItemSet* body = new ItemSet();
-		ItemSet* head = new ItemSet();
-		size_t bid, hid;
+    head_elems = odbc_conn->prepareStatement(
+    "SELECT * FROM "+ connection.getTableName(Connection::HeadsTable) +
+    " WHERE id=?");
 
-		bid = rs_rules->getInt(1);
-		hid = rs_rules->getInt(2);
-		
-		readElems( bid, *body, body_elems );
-		readElems( hid, *head, head_elems );
-		
-		r.setBody(body);
-		r.setHead(head);
-		r.setSupport(rs_rules->getFloat(3));
-		r.setConfidence(rs_rules->getFloat(4));
-		r.setBodyId(bid);
-		r.setHeadId(hid);
-	}
+
+    MRDebug() << "QueryResult::Iterator, the filter query is:" << query << std::endl;
+
+    rs_rules = state->executeQuery(query);
+  }
+
+  bool QueryResult::Iterator::next() {
+    return rs_rules->next();
+  }
+
+  void QueryResult::Iterator::getRule( Rule& r ) throw(MineruleException, odbc::SQLException, std::exception) {
+    ItemSet* body = new ItemSet();
+    ItemSet* head = new ItemSet();
+    size_t bid, hid;
+
+    bid = rs_rules->getInt(1);
+    hid = rs_rules->getInt(2);
+
+    readElems( bid, *body, body_elems );
+    readElems( hid, *head, head_elems );
+
+    r.setBody(body);
+    r.setHead(head);
+    r.setSupport(rs_rules->getFloat(3));
+    r.setConfidence(rs_rules->getFloat(4));
+    r.setBodyId(bid);
+    r.setHeadId(hid);
+  }
 
 } // namespace
